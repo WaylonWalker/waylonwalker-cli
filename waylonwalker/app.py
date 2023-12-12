586702ccaf511f5a1309d0cda8c73325f006e2c2
Waylon S. Walker <waylon@waylonwalker.com>
2023-12-12 12:16:48 -0600

    include thoughts

--- waylonwalker/app.py
+++ waylonwalker/app.py
@@ -1,16 +1,13 @@
 import webbrowser
 
 from textual.app import App, ComposeResult
-from textual.containers import Container
+from textual.containers import Container, ScrollableContainer
 from textual.css.query import NoMatches
 from textual.message import Message
-from textual.widgets import Footer, Header, Static
+from textual.widgets import Footer, Header, Static, MarkdownViewer, Markdown
+import httpx
 
 LINKS = [
-    (
-        "neovimconf2022-slides",
-        "https://neovimconf2022.waylonwalker.com/extending-vim/slide-0/",
-    ),
     ("home", "https://waylonwalker.com/"),
     ("blog", "https://waylonwalker.com/archive/"),
     ("YouTube", "https://youtube.com/waylonwalker"),
@@ -20,9 +17,37 @@ LINKS = [
     ("LinkedIn", "https://www.linkedin.com/in/waylonwalker/"),
 ]
 
+THOUGHTS = [
+    (
+        thought["title"][:50],
+        f'https://thoughts.waylonwalker.com/post/{thought["id"]}',
+        thought,
+    )
+    for thought in httpx.get(
+        "https://thoughts.waylonwalker.com/posts/waylonwalker/?page_size=99999"
+    ).json()
+]
+
+
+class Post(Markdown):
+    def __init__(self, post):
+        super().__init__(post[2]["message"])
+        self.post = post[2]
+        self.set_message()
+
+    def set_post(self, index):
+        self.post = THOUGHTS[index][2]
+        self.set_message()
+
+    def set_message(self):
+        self.message = (
+            f'# {self.post["title"]}\n{self.post["link"]}\n\n{self.post["message"]}'
+        )
+        self.update(self.message)
+
 
 class Link(Static):
-    def __init__(self, title, url):
+    def __init__(self, title, url, thought=None):
         super().__init__(title)
         self.title = title
         self.url = url
@@ -37,7 +62,8 @@ class Link(Static):
         self.add_class("active")
 
     async def on_leave(self):
-        await self.emit(self.ClearActive(self))
+        self.remove_class("active")
+        # await self.emit(self.ClearActive(self))
 
 
 class WaylonWalker(App):
@@ -56,8 +82,109 @@ class WaylonWalker(App):
     }
 
     #about {
+
+        border: round white;
         color: $text-muted;
             }
+    .mt-1 {
+        margin-top: 1;
+    }
+    .mt-2 {
+        margin-top: 2;
+    }
+    .mt-3 {
+        margin-top: 3;
+    }
+
+    .my-1 {
+        margin-top: 1;
+        margin-bottom: 1;
+    }
+    .py-1 {
+        padding-top: 1;
+        padding-bottom: 1;
+    }
+    .my-2 {
+        margin-top: 2;
+        margin-bottom: 2;
+    }
+    .py-2 {
+        padding-top: 2;
+        padding-bottom: 2;
+    }
+    .my-3 {
+        margin-top: 3;
+        margin-bottom: 3;
+    }
+
+    .max-w-3xl {
+        max-width: 80;
+    }
+
+    .font-bold {
+        text-style: bold;
+    }
+
+    .bg-blue {
+        background: $accent;
+    }
+
+    .top-0 {
+        dock: top;
+    }
+
+    .border-1 {
+        border: wide black;
+    }
+
+    .border-2 {
+        border: thick black;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+
+    .center {
+        align: center middle;
+        content-align: center middle;
+    }
+
+    #posts {
+        width: 1fr;
+        height: 1fr;
+        border: solid white;
+    }
+    #links {
+        width: 1fr;
+        height: 1fr;
+        border: solid white;
+    }
+
+    #content-grid {
+    layout: grid;
+    grid-size: 3 1;
+    grid-rows: 1fr;
+    grid-columns: 20 1fr 1fr;
+    grid-gutter: 1;
+
+    }
+
+    #content {
+     layout: grid;
+    grid-size: 1 2;
+    grid-rows: 8 1fr;
+
+    }
+
+    #post {
+            margin-top: 0;
+            height: 100%;
+            }
+
+
+
+
     """
     BINDINGS = [
         ("ctrl+c", "quit", "Quit"),
@@ -67,6 +194,12 @@ class WaylonWalker(App):
         ("down", "next", "Next"),
         ("k", "previous", "Prev"),
         ("up", "previous", "Prev"),
+        ("h", "left", "Left"),
+        ("left", "left", "Left"),
+        ("l", "right", "Right"),
+        ("u", "page_up", "Page Up"),
+        ("d", "page_down", "Page Down"),
+        ("right", "right", "Right"),
         ("enter", "open", "Open Link"),
         ("space", "open", "Open Link"),
     ]
@@ -82,7 +215,13 @@ class WaylonWalker(App):
     def action_previous(self):
         self.select(-1)
 
-    def select(self, n):
+    def action_page_down(self):
+        self.select(5)
+
+    def action_page_up(self):
+        self.select(-5)
+
+    def action_left(self):
         links = self.query("Link")
         try:
             active = self.query_one(".active")
@@ -90,7 +229,33 @@ class WaylonWalker(App):
             links[0].add_class("active")
             return
         active_index = links.nodes.index(active)
-        next_index = active_index + n
+        if active_index > len(LINKS):
+            self.select(active_index - len(LINKS), absolute=True)
+
+    def action_right(self):
+        links = self.query("Link")
+        try:
+            active = self.query_one(".active")
+        except NoMatches:
+            links[0].add_class("active")
+            return
+        active_index = links.nodes.index(active)
+        if active_index < len(LINKS):
+            self.select(len(LINKS) + active_index, absolute=True)
+
+    def select(self, n=1, absolute=False):
+        links = self.query("Link")
+        post = self.query("Post").first()
+        try:
+            active = self.query_one(".active")
+        except NoMatches:
+            links[0].add_class("active")
+            return
+        active_index = links.nodes.index(active)
+        if absolute:
+            next_index = n
+        else:
+            next_index = active_index + n
         if next_index >= len(links):
             next_index = 0
         elif next_index < 0:
@@ -100,6 +265,8 @@ class WaylonWalker(App):
         links[next_index].add_class("active")
         active = self.query_one(".active")
         active.scroll_visible()
+        if next_index > len(LINKS) - 1:
+            post.set_post(next_index - len(LINKS))
 
     def on_link_clear_active(self):
         for node in self.query(".active").nodes:
@@ -110,10 +277,41 @@ class WaylonWalker(App):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Static(
-            "Hey, Im Waylon.  Join me at Neovimconf Friday Dec, 9 1:15cst", id="about"
+        yield ScrollableContainer(
+            Container(
+                Static(
+                    "Hey, Im Waylon.  Husband, Father of two beautiful children, Senior Python Developer currently working in the Data Engineering platform space. I am a continuous learner, and share my learning in public.",
+                    id="about",
+                    classes="max-w-3xl my-1 py-1",
+                ),
+                id="about-container",
+                classes="center",
+            ),
+            Container(
+                ScrollableContainer(
+                    Static(
+                        "Social Links",
+                        classes="top-0 py-1 text-center border-1 bg-blue font-bold",
+                    ),
+                    *[Link(*link) for link in LINKS],
+                    id="links",
+                ),
+                ScrollableContainer(
+                    Static(
+                        "Thoughts from thoughts.waylonwalker.com",
+                        classes="top-0 py-1 text-center border-1 bg-blue font-bold",
+                    ),
+                    *[Link(*link) for link in THOUGHTS],
+                    id="posts",
+                ),
+                ScrollableContainer(
+                    Post(THOUGHTS[0]),
+                    id="post",
+                ),
+                id="content-grid",
+            ),
+            id="content",
         )
-        yield Container(*[Link(*link) for link in LINKS])
         yield Footer()
 
 
